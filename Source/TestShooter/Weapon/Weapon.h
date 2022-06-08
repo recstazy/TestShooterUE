b836d29a4a3f154054d41ac804f8ba27e7d451f3
Stanislav Bukotkin <djexstas9@gmail.com>
2022-06-09 01:22:12 +0600

    Add weapon interface

---
+++ Source/TestShooter/Weapon/Weapon.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "Weapon.generated.h"
+
+UINTERFACE()
+class UWeapon : public UInterface
+{
+	GENERATED_BODY()
+};
+
+class TESTSHOOTER_API IWeapon
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION()
+	virtual void Shoot() = 0;
+	virtual void Reload() = 0;
+};
